576a6d0f9605f4dc35b01e49febcf74046a363a5
Ekaterina Voronina <vor@altlinux.org>
2023-07-31 19:09:33 +0400

    fix: switch hrefs  to pointers

--- heuristic.h
+++ heuristic.h
@@ -2,8 +2,8 @@
 #define HEURISTIC_H
 #include "node.h"
 
-int heuristic_Manhattan(Node &v1, Node &v2);
-int heuristic_Chebyshev(Node &v1, Node &v2);
-int heuristic_Euclid(Node &v1, Node &v2);
+int heuristic_Manhattan(Node *v1, Node *v2);
+int heuristic_Chebyshev(Node *v1, Node *v2);
+int heuristic_Euclid(Node *v1, Node *v2);
 
 #endif // HEURISTIC_H
