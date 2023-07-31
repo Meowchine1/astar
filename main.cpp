3f1bc4bc34e5ea22fd2773a9a6c39ba5621659c7
Meowchine1 <katevoronina128@gmail.com>
2023-07-31 23:40:56 +0300

    add: astar testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "node.h"
 #include "graph.h"
+#include "astar.h"
 #include <iostream>
 #include <filesystem>
 namespace fs = std::filesystem;
@@ -10,12 +11,21 @@ int main(int argc, char *argv[])
 //            .append("../astar/data/graph.txt");
 //    std::cout<<path;
 //    Graph graph(path);
-//    graph.printGraph();
-    Node elem1("a", 12, 111), elem2("b", -114, 59);
+
+
+
+    Node a("a", 0, 0), b("b", 2, 0), c("c", 5, 3),
+            d("d", 0, 5), e("e", 5, 5);
     Graph graph;
-    graph.set_relation(&elem1, &elem2, 45);
-    graph.printGraph();
-    std::cout<< "\n weight = " <<graph.get_edge_weight(&elem1, &elem2) << std::endl;
+    graph.set_relation(&a, &b, 30);
+    graph.set_relation(&a, &e, 100);
+    graph.set_relation(&a, &d, 40);
+    graph.set_relation(&d, &e, 40);
+    graph.set_relation(&b, &c, 10);
+    graph.set_relation(&c, &e, 10);
+    //std::cout<< "\n weight = " <<graph.get_edge_weight(&elem1, &elem2) << std::endl;
+    Astar astar;
+    std::cout << astar.run(&a, &e, graph);
 
     return 0;
 }
