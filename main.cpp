c98d2290de26dec7e32d8a1b45e55d6601bcdae6
Ekaterina Voronina <vor@altlinux.org>
2023-07-31 19:24:36 +0400

    tested functions

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@ namespace fs = std::filesystem;
 
 int main(int argc, char *argv[])
 {
-    std::string path = fs::current_path()
-            .append("../astar/data/graph.txt");
-    std::cout<<path;
-    Graph graph(path);
+//    std::string path = fs::current_path()
+//            .append("../astar/data/graph.txt");
+//    std::cout<<path;
+//    Graph graph(path);
+//    graph.printGraph();
+    Node elem1("a", 12, 111), elem2("b", -114, 59);
+    Graph graph;
+    graph.set_relation(&elem1, &elem2, 45);
     graph.printGraph();
+    std::cout<< "\n weight = " <<graph.get_edge_weight(&elem1, &elem2) << std::endl;
+
     return 0;
 }
