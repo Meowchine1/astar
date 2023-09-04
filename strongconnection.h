b0a2f3004d0edfcf9091273cc0e720ede6a2df0a
Ekaterina Voronina <vor@altlinux.org>
2023-09-04 14:17:14 +0400

    1d

--- strongconnection.h
+++ strongconnection.h
@@ -137,11 +137,10 @@ public:
         }
         for (int i = 1; i < N-2; i++){  //  left line
             for(int j = 0; j < 1; j++){
-                Node* node = mass[i][j];
-
-                Node* node1 = mass[i-1][j];
-                Node* node2 = mass[i][j+1];
-                Node* node3 = mass[i+1][j];
+                Node* node = mass[i * N + j].get();
+                Node* node1 = mass[(i-1) * N + j].get();
+                Node* node2 = mass[i * N + j + 1].get();
+                Node* node3 = mass[(i+1) * N + j].get();
                 if(!node1->isolated){
                     node->children.push_back(node1);
                 }
@@ -156,7 +155,6 @@ public:
         for (int i = N-1; i < N; i++){  //  bottom line
             for(int j = 1; j < N-2; j++){
                 Node* node = mass[i][j];
-
                 Node* node1 = mass[i-1][j];
                 Node* node2 = mass[i][j+1];
                 Node* node3 = mass[i][j-1];
